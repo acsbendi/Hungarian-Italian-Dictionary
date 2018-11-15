327c02c070c34c123a4ebd2e0cef9ea08499a379
acsbendi <acsbendi28@gmail.com>
2018-11-15 20:42:42 +0100

    Implemented basic entities.

---
+++ app/src/main/java/hu/bme/aut/shoppinglist/data/HungarianWord.java
@@ -0,0 +1,23 @@
+package hu.bme.aut.shoppinglist.data;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+import static android.arch.persistence.room.ForeignKey.CASCADE;
+
+@Entity(tableName = "hungarianword",
+        foreignKeys = @ForeignKey(entity = ItalianWord.class,
+        parentColumns = "id",
+        childColumns = "italianid",
+        onDelete = CASCADE))
+public class HungarianWord {
+
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
+    public Long id;
+
+    @ColumnInfo(name = "word")
+    public String word;
+}
