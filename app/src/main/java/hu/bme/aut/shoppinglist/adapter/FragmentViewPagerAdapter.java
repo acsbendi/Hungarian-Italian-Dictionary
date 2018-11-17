97fc1ce150fa87ce24b98eeadae336552da0cc0d
acsbendi <acsbendi28@gmail.com>
2018-11-17 18:21:21 +0100

    Added fragment view pager adapter.

---
+++ app/src/main/java/hu/bme/aut/shoppinglist/adapter/FragmentViewPagerAdapter.java
@@ -0,0 +1,46 @@
+package hu.bme.aut.shoppinglist.adapter;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import hu.bme.aut.shoppinglist.R;
+
+public class FragmentViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final Context context;
+
+    public FragmentViewPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i) {
+            case 0:
+                return new FragmentOne();
+            case 1:
+                return new FragmentTwo();
+            default:
+                return new FragmentOne();
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position==0) {
+            return context.getResources().getString(R.string.translation);
+        } else {
+            return context.getResources().getString(R.string.quiz);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
