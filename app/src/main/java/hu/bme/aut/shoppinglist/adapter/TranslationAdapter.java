24d56f6ae177bae174388c0eef13dc623f6ff2d5
acsbendi <acsbendi28@gmail.com>
2018-11-18 19:28:52 +0100

    Changed TranslationAdapter to handle favorite words.

--- app/src/main/java/hu/bme/aut/shoppinglist/adapter/TranslationAdapter.java
+++ app/src/main/java/hu/bme/aut/shoppinglist/adapter/TranslationAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,8 +17,16 @@ import hu.bme.aut.shoppinglist.data.Word;
 public class TranslationAdapter
         extends RecyclerView.Adapter<TranslationAdapter.TranslationViewHolder> {
 
-    private final List<Word> words = new ArrayList<>();
+    public interface WordFavoriteSetListener{
+        void onWordChanged(Word word);
+    }
 
+    private final List<Word> words = new ArrayList<>();
+    private WordFavoriteSetListener listener;
+
+    public TranslationAdapter(WordFavoriteSetListener listener) {
+        this.listener = listener;
+    }
 
     @NonNull
     @Override
@@ -32,6 +41,7 @@ public class TranslationAdapter
     public void onBindViewHolder(@NonNull TranslationViewHolder holder, int position) {
         Word word = words.get(position);
         holder.wordTextView.setText(word.word);
+        holder.favoriteCheckBox.setChecked(word.favorite);
 
         holder.word = word;
     }
@@ -49,12 +59,24 @@ public class TranslationAdapter
     class TranslationViewHolder extends RecyclerView.ViewHolder {
 
         TextView wordTextView;
+        CheckBox favoriteCheckBox;
 
         Word word;
 
         TranslationViewHolder(View itemView) {
             super(itemView);
             wordTextView = itemView.findViewById(R.id.wordTextView);
+            favoriteCheckBox = itemView.findViewById(R.id.favoriteCheckBox);
+
+            favoriteCheckBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(word != null){
+                        word.favorite = favoriteCheckBox.isChecked();
+                        listener.onWordChanged(word);
+                    }
+                }
+            });
         }
     }
 }
