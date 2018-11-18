71e694583b7442694a75b6b9b5fd95164910ec72
acsbendi <acsbendi28@gmail.com>
2018-11-18 20:31:38 +0100

    Added new favorite field in word to queries.

--- app/src/main/java/hu/bme/aut/shoppinglist/data/TranslationDao.java
+++ app/src/main/java/hu/bme/aut/shoppinglist/data/TranslationDao.java
@@ -10,14 +10,4 @@ public interface TranslationDao {
 
     @Insert
     void insert(Translation translation);
-
-    @Query("SELECT * FROM italianword INNER JOIN translation ON " +
-            "italianword.id=translation.italianWordId WHERE " +
-            "translation.hungarianWordId=:hungarianId")
-            List<ItalianWord> getItalianTranslationsFor(final int hungarianId);
-
-    @Query("SELECT * FROM hungarianword INNER JOIN translation ON " +
-            "hungarianword.id=translation.hungarianWordId WHERE " +
-            "translation.italianWordId=:italianId")
-            List<HungarianWord> getHungarianTranslationsFor(final int italianId);
 }
